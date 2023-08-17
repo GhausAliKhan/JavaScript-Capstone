a77e718f1a135700ed9f2f9cabe6d94c3687b25c
mwovi <petermwovi73@gmail.com>
2023-08-17 18:52:07 +0300

    Edit index.js file to add comment display section

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import './style.css';
 import icon from './assets/pageIcon.png';
 import { fetchApi } from './modules/Api.js';
 
+const container = document.getElementById('cardContainer');
 const headerIcon = document.querySelector('#pageIcon');
 headerIcon.src = icon;
 headerIcon.classList.add('icon');
+
+container.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON' && e.target.textContent === 'Comment') {
+    displayMenu(e.target.id);
+  }
+});
 
 window.onload = fetchApi();
