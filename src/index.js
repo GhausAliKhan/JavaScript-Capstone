cb143d436d3b30762db34cf116c3575809b70ccb
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-08-17 13:56:22 +0500

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+import icon from './assets/pageIcon.png';
+import { fetchApi } from './modules/Api.js';
+
+const headerIcon = document.querySelector('#pageIcon');
+headerIcon.src = icon;
+headerIcon.classList.add('icon');
+
+window.onload = fetchApi();
